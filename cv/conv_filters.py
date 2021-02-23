d1d75d03c809841b961cb80366c2c3012606e419
OwenHoffend <mcnewomaster@gmail.com>
2021-02-22 17:07:51 -0800

    Support for bipolar SN generation. Started sc_convolve simulation.

--- cv/conv_filters.py
+++ cv/conv_filters.py
@@ -1,5 +1,9 @@
 import numpy as np
 import img_io
+from sys import path
+from os.path import dirname as dir
+path.append(dir(path[0]))
+import sim.bitstreams as bitstreams
 from scipy import signal
 
 """Some common filter kernels
@@ -46,8 +50,32 @@ def convolve(img, kernel):
     npk = np.array(kernel).astype(np.float64) #Convert the kernel to a np array of float64s
     return signal.convolve2d(img, npk) #Scipy discrete convolution. boundary=fill, fillvalue=0
 
+def sc_convolve(img, kernel, bs_len=255):
+    """Implementation of a generic convolution kernel with SC."""
+
+    #Convert the image to a stochastic representation
+    rng = bitstreams.SC_RNG()
+    img_bs = img_io.img_to_bs(img, rng.bs_lfsr, bs_len=bs_len) #Correlated
+    rng.reset()
+
+    #Convert the kernel to a stochastic representation
+    npk = np.array(kernel).astype(np.float64)
+    abs_npk = np.absolute(npk) #Sign is handled seperately
+    kern_img = 255.0 * abs_npk / np.max(abs_npk) #Format kernel as an image with values in the range of 0 to 255
+    kern_bs = img_io.img_to_bs(kern_img, rng.bs_lfsr, bs_len=bs_len)
+
+    #Perform the convolution
+    i_h, i_w = img.shape
+    k_h, k_w = npk.shape
+    for i in range(i_h):
+        for j in range(i_w):
+            pass #TODO: Finish
+
 if __name__ == "__main__":
     """Perform basic edge detection on a test image"""
-    lena = img_io.load_img("./img/lena256.png", gs=True) #Load test image as greyscale
+    lena = img_io.load_img("../img/lena256.png", gs=True) #Load test image as greyscale
     lena_ed = convolve(lena, EDGE_DETECT_8)
     img_io.disp_img(lena_ed)
+
+    """Test of sc_convolve"""
+    sc_convolve(lena, EDGE_DETECT_8)
--- sim/bitstreams.py
+++ sim/bitstreams.py
@@ -10,6 +10,16 @@ class SC_RNG:
         self.u = None
         self.lfsr = None
 
+    def _run_lfsr(self, n, lfsr_sz, keep_rng=True):
+        if not keep_rng or self.lfsr is None:
+            L = LFSR()
+            fpoly = L.get_fpolyList(m=lfsr_sz)[0] #The 0th index holds the lfsr poly with the fewest xor gates
+            L = LFSR(fpoly=fpoly, initstate='random')
+            self.lfsr = np.zeros(n, dtype=np.uint32) #TODO: Can the dtype specifier be removed?
+            for i in range(n):
+                L.runKCycle(1)
+                self.lfsr[i] = bit_vec_to_int(L.state)
+
     def bs_uniform(self, n, p, keep_rng=True):
         """Return a numpy array containing a unipolar stochastic bistream
             with value p based on n numbers drawn from the uniform random distribution on [0, 1).
@@ -21,17 +31,22 @@ class SC_RNG:
 
     def bs_lfsr(self, n, p, keep_rng=True): #Warning: when keep_rng=False, runtime is very slow 
         """Generate a stochastic bitstream using an appropriately-sized simulated LFSR"""
-        if not keep_rng or self.lfsr is None:
-            lfsr_sz = int(np.ceil(np.log2(n)))
-            L = LFSR()
-            fpoly = L.get_fpolyList(m=lfsr_sz)[0] #The 0th index holds the lfsr poly with the fewest xor gates
-            L = LFSR(fpoly=fpoly, initstate='random')
-            self.lfsr = np.zeros(n, dtype=np.uint32)
-            for i in range(n):
-                #TODO: Add a method to skip values in the LFSR spectrum that aren't used
-                L.runKCycle(1)
-                self.lfsr[i] = bit_vec_to_int(L.state)
-        return np.packbits(self.lfsr / n <= p)
+        lfsr_sz = int(np.ceil(np.log2(n)))
+        self._run_lfsr(n, lfsr_sz, keep_rng=keep_rng)
+        return np.packbits(self.lfsr / (2**lfsr_sz-1) <= p)
+
+    def up_to_bp_lfsr(self, n, up, keep_rng=True):
+        """Map a unipolar SN in the range [0, 1] onto a bipolar one on [0.5, 1]"""
+        lfsr_sz = int(np.ceil(np.log2(2*n))) #Generate an LFSR that is slightly too large
+        self._run_lfsr(n, lfsr_sz, keep_rng=keep_rng)
+        msb_mask = 1 << lfsr_sz - 1
+        bp = (self.lfsr & np.full((1, n), ~msb_mask)) / (2**(lfsr_sz-1)) <= up
+        return np.packbits(bp | (self.lfsr & np.full((1, n), msb_mask) == msb_mask))
+
+    def bs_bp_lfsr(self, n, bp, keep_rng=True):
+        """Generate a bipolar stochastic bitstream via the bs_lfsr or bs_uniform method. Can be correlated"""
+        up = (bp + 1.0) / 2.0
+        return self.bs_lfsr(n, bp, keep_rng=keep_rng)
 
 def bit_vec_to_int(vec):
     """Utility function for converting a np array bit vector to an integer"""
@@ -48,6 +63,11 @@ def bs_mean(bs):
     unp = np.unpackbits(bs)
     return np.mean(unp)
 
+def bs_mean_bp(bs):
+    """Evaluate the bipolar probability value of a bitstream"""
+    m = bs_mean(bs) #Unipolar mean
+    return 2.0 * m - 1.0
+
 def bs_scc(bsx, bsy):
     """Compute the stochastic cross-correlation between two bitstreams according to Eq. (1)
     in [A. Alaghi and J. P. Hayes, Exploiting correlation in stochastic circuit design]"""
@@ -59,3 +79,8 @@ def bs_scc(bsx, bsy):
         return (p_actual - p_uncorr) / (np.minimum(px, py) - p_uncorr)
     else:
         return (p_actual - p_uncorr) / (p_uncorr + np.maximum(px + py - 1, 0))
+
+if __name__ == "__main__":
+    """Test bs_bp_lfsr"""
+    rng = SC_RNG()
+    print(bs_mean_bp(rng.up_to_bp_lfsr(256, 0.25))) #Play with the values here
